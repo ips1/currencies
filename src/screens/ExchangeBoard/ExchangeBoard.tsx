78f566380ef66bbd1131a62fa680a0890fbd3367
petrk <petrk@salsitasoft.com>
2023-12-17 23:33:52 +0100

    Improve loading and error screens

--- src/screens/ExchangeBoard/ExchangeBoard.tsx
+++ src/screens/ExchangeBoard/ExchangeBoard.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useCallback } from 'react'
-import { ActivityIndicator, FlatList, Text, TouchableHighlight } from 'react-native'
-import { useTheme } from 'styled-components/native'
+import { ActivityIndicator, Button, FlatList, SafeAreaView, TouchableHighlight } from 'react-native'
+import styled, { useTheme } from 'styled-components/native'
+import { BaseText } from '../../components/BaseText.tsx'
 import { useCurrentExchangeBoard } from '../../hooks/useCurrentExchangeBoard.ts'
 import { ForeignCurrency, SupportedLocalCurrencyCode } from '../../model/types.ts'
 import { ExchangeBoardNavigationProps } from '../../navigation/params.ts'
@@ -10,6 +11,12 @@ import { ExchangeBoardItem } from './components/ExchangeBoardItem.tsx'
 type ExchangeBoardProps = ExchangeBoardNavigationProps & {
   localCurrency: SupportedLocalCurrencyCode
 }
+
+const FullScreenWrapper = styled.SafeAreaView`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`
 
 export const ExchangeBoard: FC<ExchangeBoardProps> = ({ localCurrency, navigation }) => {
   const queryResult = useCurrentExchangeBoard(localCurrency)
@@ -26,30 +33,40 @@ export const ExchangeBoard: FC<ExchangeBoardProps> = ({ localCurrency, navigatio
   )
 
   if (queryResult.isLoading) {
-    return <ActivityIndicator />
+    return (
+      <FullScreenWrapper>
+        <ActivityIndicator />
+      </FullScreenWrapper>
+    )
   }
 
   if (!queryResult.data) {
     // TODO: Figure out & make error nicer
     // TODO: Retry initialization?
-    const error = queryResult.error?.toString() ?? 'Unknown error'
-    return <Text>{error}</Text>
+    return (
+      <FullScreenWrapper>
+        <BaseText>Unable to retrieve the rates data</BaseText>
+        <Button title={'Retry'} onPress={() => queryResult.refetch()} />
+      </FullScreenWrapper>
+    )
   }
 
   return (
-    <FlatList
-      data={queryResult.data.currencies}
-      refreshing={queryResult.isRefetching}
-      onRefresh={queryResult.refetch}
-      renderItem={(item) => (
-        <TouchableHighlight
-          underlayColor={theme.colors.inputBackground}
-          key={item.item.currencyCode}
-          onPress={() => navigateToDetail(item.item)}
-        >
-          <ExchangeBoardItem foreignCurrency={item.item} localCurrencyCode={localCurrency} />
-        </TouchableHighlight>
-      )}
-    />
+    <SafeAreaView>
+      <FlatList
+        data={queryResult.data.currencies}
+        refreshing={queryResult.isRefetching}
+        onRefresh={queryResult.refetch}
+        renderItem={(item) => (
+          <TouchableHighlight
+            underlayColor={theme.colors.inputBackground}
+            key={item.item.currencyCode}
+            onPress={() => navigateToDetail(item.item)}
+          >
+            <ExchangeBoardItem foreignCurrency={item.item} localCurrencyCode={localCurrency} />
+          </TouchableHighlight>
+        )}
+      />
+    </SafeAreaView>
   )
 }
